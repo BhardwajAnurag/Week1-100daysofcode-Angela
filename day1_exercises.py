b45363f8160f2bafdd08d83293359c57a87049e6
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-30 19:22:45 +0000

    project1 band name generator

--- day1_exercises.py
+++ day1_exercises.py
@@ -1,10 +1,34 @@
-#Write your code below this line 👇
+print("Exercise 1 - Printing\n")
 print("""Day 1 - Python Print Function
 The function is declared like this:
-print('what to print')""")
-print()
+print('what to print')\n""")
+
+print("Exercise 2 - Debugging Practice\n")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the ''"+"'' sign.')
+print('e.g. print("Hello "' ' + ' '"world")')
+print("New lines can be created with a backslash and n.\n")
+
 print("Exercise 3 - Input Function\n")
 # name = input("Please tell your name and I will tell you how many digits your name has:\n")
 # print(f"Your name has {len(name)} letters")
-
 print(len(input("What is your name?")))
+
+print()
+print("Exercise 4 - Variables\n")
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+c=a
+a=b
+b=c
+#Write your code above this line 👆
+####################################
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+print()
---
+++ project1_band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator")
+
+city = input("Where did you grow up?\n")
+
+pet = input("What is your pet called?\n")
+
+print(f"Your band name would be {city} {pet}")
