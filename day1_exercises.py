88d0a9edc13416adfd85dab3f69bdd9b1efc805a
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-30 18:44:08 +0000

    Initial commit

---
+++ day1_exercises.py
@@ -0,0 +1,10 @@
+#Write your code below this line 👇
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
+print()
+print("Exercise 3 - Input Function\n")
+# name = input("Please tell your name and I will tell you how many digits your name has:\n")
+# print(f"Your name has {len(name)} letters")
+
+print(len(input("What is your name?")))
